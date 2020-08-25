c8724a087a7cc7b74125a2268648c04d29283c1a
Queen of Corgis <tana.owens@gmail.com>
2020-08-24 20:54:50 -0600

    search form appears

--- reactuserdirectory/src/App.js
+++ reactuserdirectory/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchForm from "./components/SearchForm"
+import EmployeeCard from "./components/EmployeeCard"
 
 function App() {
   return (
     <div className="App">
-     
+     <SearchForm />
+     <EmployeeCard />
     </div>
   );
 }
